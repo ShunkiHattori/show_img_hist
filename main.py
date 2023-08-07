07aebfb2efb3f2e4666b0d27f6212fbc56aa9c82
Shunki Hattori <s.hattori.569@stn.nitech.ac.jp>
2023-08-08 00:47:28 +0900

    fix the error

--- main.py
+++ main.py
@@ -1,38 +1,83 @@
-import numpy as np
-import pandas as pd
-import streamlit as st
-from skimage.io import imread
+import func
+
+print("1.西暦を和暦に変換")
+print("2.西暦、月、日からその日の曜日と祝日かどうかを出力")
+print("3.西暦、月からその月の祝日を検索")
+sel = 0
+print("")
+while True:
+    try:
+        sel = int(input("使う機能の番号を入力してください(半角):"))
+    except ValueError:
+        print("半角数字で入力してください")
+    if sel != 1 and sel != 2 and sel != 3:
+        print("1,2,3から選んでください")
+    if sel == 1 or sel == 2 or sel == 3:
+        break
 
 
-# download the image
-img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
+if sel == 1:
+    print("")
+    print("西暦を和暦に変換します")
+    AD = int(input("西暦を入力してください(半角で1867以上2023以下の数字):"))
+    print(func.ADtrans(AD))
 
-im = imread(img_url))
+if sel == 2:
+    print("")
+    print("西暦、月、日からその日の曜日と祝日かどうかを出力します")
+    while True:
+        try:
+            AD = int(input("西暦を入力してください(半角):"))
+        except ValueError:
+            print("半角数字で入力してください")
+        else:
+            break
 
-st.image(im, caption='image from wikimedia commons',
-         use_column_width=True)
+    while True:
+        try:
+            month = int(input("月を入力してください(半角):"))
+        except ValueError:
+            print("半角数字で入力してください")
+        else:
+            if month > 12 or month < 1:
+                print("1～12から選んでください")
+            else:
+                break
 
+    while True:
+        try:
+            day = int(input("日を入力してください(半角):"))
+        except ValueError:
+            print("半角数字で入力してください")
+        else:
+            if day > 31 or day < 1:
+                print("1～31から選んでください")
+            else:
+                break
+    try:
+        func.week(AD, month, day)
+    except ValueError:
+        print("その日付は存在しません")
 
-# show histgram of all colors
-hist_red, _ = np.histogram(im[:, :, 0], bins=64)
-hist_green, _ = np.histogram(im[:, :, 1], bins=64)
-hist_blue, _ = np.histogram(im[:, :, 2], bins=64)
-hist = np.stack((hist_red, hist_green, hist_blue), axis=1)
+if sel == 3:
+    print("")
+    print("西暦、月からその月の祝日を検索します")
+    while True:
+        try:
+            AD = int(input("西暦を入力してください(半角):"))
+        except ValueError:
+            print("半角数字で入力してください")
+        else:
+            break
 
-df_hist = pd.DataFrame(hist, columns=['R', 'G', 'B'])
-st.bar_chart(df_hist)
-
-
-# choose one color
-color = st.radio(
-    "choose R, G, or B",
-    ('R', 'G', 'B'))
-if color == 'R':
-    df_hist = pd.DataFrame(hist_red)
-    st.bar_chart(df_hist)
-if color == 'G':
-    df_hist = pd.DataFrame(hist_green)
-    st.bar_chart(df_hist)
-if color == 'B':
-    df_hist = pd.DataFrame(hist_blue)
-    st.bar_chart(df_hist)
+    while True:
+        try:
+            month = int(input("月を入力してください(半角):"))
+        except ValueError:
+            print("半角数字で入力してください")
+        else:
+            if month > 12 or month < 1:
+                print("1～12から選んでください")
+            else:
+                break
+    func.holiday(AD, month)
